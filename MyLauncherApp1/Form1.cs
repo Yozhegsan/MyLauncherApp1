9661330f0108cd97a3df30c032df4f5ac6c902a4
Олександр Гнідін <yozhegsan@gmail.com>
2019-10-30 12:17:15 +0200

    Add WindowsHookEx. Worked!!!

--- MyLauncherApp1/Form1.cs
+++ MyLauncherApp1/Form1.cs
@@ -13,48 +13,39 @@ namespace MyLauncherApp1
 {
     public partial class Form1 : Form
     {
+        //-------- SetWindowsHookEx --------------------------------------------------------------------------------------------------------
         [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
         private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
-
         [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
         private static extern bool UnhookWindowsHookEx(IntPtr hhk);
-
         [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
         private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
-
         [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
         private static extern IntPtr GetModuleHandle(string lpModuleName);
-
         private const int WH_KEYBOARD_LL = 13;
         private const int WM_KEYDOWN = 0x0100;
         private static LowLevelKeyboardProc _proc = HookCallback;
         private static IntPtr _hookID = IntPtr.Zero;
-
-
         private static IntPtr SetHook(LowLevelKeyboardProc proc)
         {
             using (Process curProcess = Process.GetCurrentProcess())
-            using (ProcessModule curModule = curProcess.MainModule)
-            {
-                return SetWindowsHookEx(WH_KEYBOARD_LL, proc, GetModuleHandle(curModule.ModuleName), 0);
-            }
+            using (ProcessModule curModule = curProcess.MainModule) { return SetWindowsHookEx(WH_KEYBOARD_LL, proc, GetModuleHandle(curModule.ModuleName), 0); }
         }
-
         private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
-
         private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
         {
             if ((nCode >= 0) && (wParam == (IntPtr)WM_KEYDOWN))
             {
                 int vkCode = Marshal.ReadInt32(lParam);
                 if (((Keys)vkCode == Keys.LWin) || ((Keys)vkCode == Keys.RWin))
-                {
-                    Console.WriteLine("{0} blocked!", (Keys)vkCode);
                     return (IntPtr)1;
-                }
             }
             return CallNextHookEx(_hookID, nCode, wParam, lParam);
         }
+        //--- Load hook       _hookID = SetHook(_proc);
+        //--- Unload hook     UnhookWindowsHookEx(_hookID);
+        //=============================================================================================================================
+
         //###########################################################################################################
 
         public Form1()
@@ -64,15 +55,10 @@ namespace MyLauncherApp1
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            _hookID = SetHook(_proc);
-            
-
+            _hookID = SetHook(_proc); //Load hook
             string[] s = Environment.GetEnvironmentVariable("path").Split(';');
-
             foreach (string ss in s)
                 listBox1.Items.Add(ss);
-
-
             picTest.Image = Icon.ExtractAssociatedIcon(@"C:\Program Files (x86)\Adobe\Adobe Photoshop CS3\Photoshop.exe").ToBitmap();
         }
 
@@ -83,7 +69,7 @@ namespace MyLauncherApp1
 
         private void Form1_FormClosed(object sender, FormClosedEventArgs e)
         {
-            UnhookWindowsHookEx(_hookID);
+            UnhookWindowsHookEx(_hookID); //Unload hook
         }
     }
 }
